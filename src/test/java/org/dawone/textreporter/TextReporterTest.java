15e8df98f735e8140b2d1dda5e6f8f0d67515345
ccorreas <correasberenguer@gmail.com>
2026-03-17 19:32:45 +0100

    feat: TextReporterTest

--- src/test/java/org/dawone/textreporter/TextReporterTest.java
+++ src/test/java/org/dawone/textreporter/TextReporterTest.java
@@ -7,7 +7,42 @@ import static org.junit.jupiter.api.Assertions.*;
 class TextReporterTest {
 
     @Test
-    void reportIsWordUsed_SiHayPalabra_DevuelveTrue() {
+    void reportIsWordUsed_SiCoincidePalabra_DevuelveTrue() {
+        String apiEndpoint = "https://uselessfacts.jsph.pl//api/v2/facts/today";
+        FactOfTheDayLineReader reader = new FactOfTheDayLineReader(apiEndpoint);
+        WordProcessor wordProcessor = new WordProcessor();
+        TextReporter textReporter = new TextReporter(reader, wordProcessor);
 
+        String wordToCheck = "the";
+        boolean isUsed = textReporter.reportIsWordUsed(wordToCheck);
+
+        assertEquals(true, isUsed);
     }
+
+    @Test
+    void reportIsWordUsed_SiNoCoincidePalabra_DevuelveFalse() {
+        String apiEndpoint = "https://uselessfacts.jsph.pl//api/v2/facts/today";
+        FactOfTheDayLineReader reader = new FactOfTheDayLineReader(apiEndpoint);
+        WordProcessor wordProcessor = new WordProcessor();
+        TextReporter textReporter = new TextReporter(reader, wordProcessor);
+
+        String wordToCheck = "asdfghjklqwertyuiop";
+        boolean isUsed = textReporter.reportIsWordUsed(wordToCheck);
+
+        assertEquals(false, isUsed);
+    }
+
+    @Test
+    void reportIsWordUsed_SiNoHayPalabra_DevuelveExcepcion() {
+        String apiEndpoint = "https://uselessfacts.jsph.pl//api/v2/facts/today";
+        FactOfTheDayLineReader reader = new FactOfTheDayLineReader(apiEndpoint);
+        WordProcessor wordProcessor = new WordProcessor();
+        TextReporter textReporter = new TextReporter(reader, wordProcessor);
+
+        String wordToCheck = " ";
+
+        assertThrows(IllegalArgumentException.class, () ->   textReporter.reportIsWordUsed(wordToCheck));
+    }
+
+
 }

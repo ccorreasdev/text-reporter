fcbf3514464efe54b4b20ffd0c33f0899701c3d9
ccorreas <correasberenguer@gmail.com>
2026-03-17 19:14:45 +0100

    feat: Añadido FactOfTheDayReaderTest metodos

--- src/test/java/org/dawone/textreporter/FactOfTheDayLineReaderTest.java
+++ src/test/java/org/dawone/textreporter/FactOfTheDayLineReaderTest.java
@@ -7,6 +7,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class FactOfTheDayLineReaderTest {
 
     @Test
-    void readLines() {
+    void readLines_SiEsHoy_DevuelveLaFrase() {
+        String apiEndpoint = "https://uselessfacts.jsph.pl//api/v2/facts/today";
+        FactOfTheDayLineReader reader = new FactOfTheDayLineReader(apiEndpoint);
+        String[] lineasLeidas = reader.readLines();
+        String expected = "The first owner of the Marlboro company died of lung cancer.";
+
+        assertEquals(expected, lineasLeidas[0]);
+    }
+
+    @Test
+    void readLines_SiNoHayEndPoint_DevuelveExcepcion() {
+        String apiEndpoint = "";
+        FactOfTheDayLineReader reader = new FactOfTheDayLineReader(apiEndpoint);
+
+        assertThrows(IllegalArgumentException.class, () -> reader.readLines());
     }
 }

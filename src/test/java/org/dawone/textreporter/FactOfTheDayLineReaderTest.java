edcdcac4a92897ccab071e7a2c4e55fab4f4ad31
ccorreas <correasberenguer@gmail.com>
2026-03-17 19:04:02 +0100

    feat: Añadido FactOfTheDayReaderTest

---
+++ src/test/java/org/dawone/textreporter/FactOfTheDayLineReaderTest.java
@@ -0,0 +1,12 @@
+package org.dawone.textreporter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FactOfTheDayLineReaderTest {
+
+    @Test
+    void readLines() {
+    }
+}
--- src/test/java/org/dawone/textreporter/TextReporterTest.java
+++
@@ -1,4 +0,0 @@
-package org.dawone.textreporter;
-
-public class TextReporterTest {
-}
